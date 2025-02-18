72ce59a3fce23f7ce78c6f41db8bf089bff2682e
felo <felo.safwat2000@gmail.com>
2025-02-18 04:34:19 +0200

    Done

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,27 +1,30 @@
 import connectDB from "./DB/db.conction.js";
-import authController from "./modules/auth/auth.controller.js"
-import usercontroller from "./modules/user/user.controller.js"
-import productController from "./modules/product/product.controller.js"
+import authController from "./modules/auth/auth.controller.js";
+import usercontroller from "./modules/user/user.controller.js";
+import productController from "./modules/product/product.controller.js";
 import { globalErrorHandling } from "./utils/error/error.js";
-import cors from "cors"
-connectDB()
+import cors from "cors";
 
-
+connectDB();
 
 const bootstrap = (app, express) => {
+    app.use(express.json());
+
     app.use(cors({
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE', "PATCH"],
-        credentials: true
+        origin: '*',  // السماح فقط من هذا الدومين
+        methods: ['GET', 'POST', 'PUT'],  // السماح بهذه الطرق
+        allowedHeaders: ['Content-Type']  // السماح بهذا الـ header
     }));
-    app.use(express.json())
+    app.options('*', cors());  // السماح بـ OPTIONS requests
     app.get('/', (req, res, next) => {
         res.send('Hello World!');
     });
-    app.use("/auth", authController)
-    app.use("/user", usercontroller)
-    app.use("/product", productController)
-    app.use(globalErrorHandling)
-}
 
-export default bootstrap
+    app.use("/auth", authController);
+    app.use("/user", usercontroller);
+    app.use("/product", productController);
+
+    app.use(globalErrorHandling);
+};
+
+export default bootstrap;
